cecd513d2960802601dea6b7b359964012754c5f
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-01-08 20:48:55 +0100

    Footer completed - responsive mobile-first

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,9 @@
 <footer>
   {{-- Footer Top --}}
   <div class="footer-top">
-    {{-- Footer Top Mobile --}}
-    <div class="footer-top-mobile text-uppercase">
-      <div class="container">
+    <div class="container">
+      {{-- Footer Top Mobile --}}
+      <div class="footer-top-mobile text-uppercase">
         <div class="row">
           <div class="col-12">
             <a href="#" class="btn-primary">Sign-up now!</a>
@@ -45,15 +45,110 @@
             </ul>
           </div>
         </div>
-      </div>  {{-- Closing Footer Top-Mobile Container --}}
-    </div>
-    {{-- Footer Top Desktop --}}
-    <div class="footer-top-desktop">
-      <div class="container">
+      </div>
+      {{-- Footer Top Desktop --}}
+      <div class="footer-top-desktop">
         <div class="row">
           <div class="col-12">
-            <div class="links">
-
+            <div class="links-wrapper">
+              <div class="links-col">
+                <div class="links-box">
+                  <h3>Dc comics</h3>
+                  <ul>
+                    <li>
+                      <a href="#">Characters</a>
+                    </li>
+                    <li>
+                      <a href="#">Comics</a>
+                    </li>
+                    <li>
+                      <a href="#">Movies</a>
+                    </li>
+                    <li>
+                      <a href="#">TV</a>
+                    </li>
+                    <li>
+                      <a href="#">Games</a>
+                    </li>
+                    <li>
+                      <a href="#">Videos</a>
+                    </li>
+                    <li>
+                      <a href="#">News</a>
+                    </li>
+                  </ul>
+                </div>
+                <div class="links-box">
+                  <h3>Shop</h3>
+                  <ul>
+                    <li>
+                      <a href="#">Shop DC</a>
+                    </li>
+                    <li>
+                      <a href="#">Shop DC collectibles</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>  {{-- Closing links-col --}}
+              <div class="links-col">
+                <div class="links-box">
+                  <h3>Dc</h3>
+                  <ul>
+                    <li>
+                      <a href="#">Terms of use</a>
+                    </li>
+                    <li>
+                      <a href="#">Privacy policy (New)</a>
+                    </li>
+                    <li>
+                      <a href="#">Advertising</a>
+                    </li>
+                    <li>
+                      <a href="#">Jobs</a>
+                    </li>
+                    <li>
+                      <a href="#">Subscriptions</a>
+                    </li>
+                    <li>
+                      <a href="#">Talent workshop</a>
+                    </li>
+                    <li>
+                      <a href="#">CPSC certificates</a>
+                    </li>
+                    <li>
+                      <a href="#">Ratings</a>
+                    </li>
+                    <li>
+                      <a href="#">Stop help</a>
+                    </li>
+                    <li>
+                      <a href="#">Contact us</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>  {{-- Closing links-col --}}
+              <div class="links-col">
+                <div class="links-box">
+                  <h3>Sites</h3>
+                  <ul>
+                    <li>
+                      <a href="#">DC</a>
+                    </li>
+                    <li>
+                      <a href="#">MAD magazine</a>
+                    </li>
+                    <li>
+                      <a href="#">DC kids</a>
+                    </li>
+                    <li>
+                      <a href="#">DC universe</a>
+                    </li>
+                    <li>
+                      <a href="#">DC power visa</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>  {{-- Closing links-col --}}
             </div>
             <div class="credits">
               <p>
@@ -68,7 +163,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </div>  {{-- Closing Footer Top Container --}}
   </div>
   {{-- Footer Bottom --}}
   <div class="footer-bottom">
