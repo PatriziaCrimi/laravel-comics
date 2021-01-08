dbafdb457ceaf047c2eb960230e17b8eee5ef51b
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-01-08 19:59:41 +0100

    Footer Bottom completed responsive mobile-first

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,76 +3,74 @@
 @section('page-title', 'Comics | DC')
 
 @section('section')
-  <section id="comics" class="text-center">
-    {{-- Current Series --}}
-    <div class="current-series">
-      <div class="container">
-        <div class="row">
-          <div class="col-12 text-left">
-            <h1>Current series</h1>
-          </div>
+  {{-- SECTION - Current Series --}}
+  <section id="current-series" class="text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-left">
+          <h1>Current series</h1>
         </div>
-        <div class="cards-wrapper">
-          <div class="row">
-            @foreach ($comics_list as $key => $album)
-              <div class="col-6 col-sm-4 col-lg-3 col-xl-2">
-                <div class="card-album">
-                  <div class="thumb-album">
-                    <img src="{{$album['thumb']}}" alt="{{$album['series']}}">
-                  </div>
-                  <h2 class="text-left">{{$album['series']}}</h2>
+      </div>
+      <div class="cards-wrapper">
+        <div class="row">
+          @foreach ($comics_list as $key => $album)
+            <div class="col-6 col-sm-4 col-lg-3 col-xl-2">
+              <div class="card-album">
+                <div class="thumb-album">
+                  <img src="{{$album['thumb']}}" alt="{{$album['series']}}">
                 </div>
+                <h2 class="text-left">{{$album['series']}}</h2>
               </div>
-            @endforeach
-          </div>
+            </div>
+          @endforeach
         </div>
-        <div class="row">
-          <div class="col-12">
-            <button class="btn-primary text-uppercase" type="button" name="button">Load more</button>
-          </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <button class="btn-primary text-uppercase" type="button" name="button">Load more</button>
         </div>
-      </div>   {{-- Closing Current-series container --}}
-    </div>
-    {{-- Links --}}
-    <div class="links">
-      <div class="container">
-        <div class="row">
-          <div class="col-12">
-            <ul class="d-flex justify-content-between">
-              <li>
-                <a href="#">
-                  <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="Digital comics">
-                  <span>Digital comics</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="Merchandise T-shirt">
-                  <span>De merchandise</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="Subscription cards">
-                  <span>Subscription</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="Locator">
-                  <span>Comic shop locator</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="Dc power visa box">
-                  <span>Dc power visa</span>
-                </a>
-              </li>
-            </ul>
-          </div>
+      </div>
+    </div>   {{-- Closing Current-series container --}}
+  </section>
+  {{-- SECTION - Links --}}
+  <section id="links" class="text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <ul class="d-flex justify-content-between">
+            <li>
+              <a href="#">
+                <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="Digital comics">
+                <span>Digital comics</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="Merchandise T-shirt">
+                <span>De merchandise</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="Subscription cards">
+                <span>Subscription</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="Locator">
+                <span>Comic shop locator</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="Dc power visa box">
+                <span>Dc power visa</span>
+              </a>
+            </li>
+          </ul>
         </div>
-      </div>  {{-- Closing Links container --}}
-    </div>
+      </div>
+    </div>  {{-- Closing Links container --}}
   </section>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,21 +1,82 @@
 <footer>
   {{-- Footer Top --}}
   <div class="footer-top">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
+    {{-- Footer Top Mobile --}}
+    <div class="footer-top-mobile text-uppercase">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <a href="#" class="btn-primary">Sign-up now!</a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <h2>Follow us</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <ul class="social-icons">
+              <li>
+                <a href="#">
+                  <img src="{{asset('img/footer-facebook.png')}}" alt="Facebook">
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="{{asset('img/footer-twitter.png')}}" alt="Twitter">
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="{{asset('img/footer-youtube.png')}}" alt="Youtube">
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="{{asset('img/footer-pinterest.png')}}" alt="Pinterest">
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="{{asset('img/footer-periscope.png')}}" alt="Periscope">
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>  {{-- Closing Footer Top-Mobile Container --}}
+    </div>
+    {{-- Footer Top Desktop --}}
+    <div class="footer-top-desktop">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="links">
 
+            </div>
+            <div class="credits">
+              <p>
+                All Site Content &#8482; and &#169; 2020 DC Entertainment, unless otherwise
+                <a href="#">noted here</a>.
+                All rights reserved.
+              </p>
+              <p>
+                <a class="text-capitalize" href="#">Cookies settings</a>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>  {{-- Closing Footer Top Container --}}
+    </div>
   </div>
   {{-- Footer Bottom --}}
   <div class="footer-bottom">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          {{-- Footer Bottom Mobile --}}
-          <div class="footer-bottom-mobile">
+    {{-- Footer Bottom Mobile --}}
+    <div class="footer-bottom-mobile">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
             <p class="text-center">
               All Site Content &#8482; and &#169; 2020 DC Entertainment, unless otherwise
               <a href="#">noted here</a>.
@@ -25,12 +86,50 @@
               <a class="text-capitalize" href="#">Cookies settings</a>
             </p>
           </div>
-          {{-- Footer Bottom Tablet --}}
-          <div class="footer-bottom-tablet">
-            <a href="#" class="btn-primary">Sign-up now!</a>
+        </div>
+      </div>  {{-- Closing Footer Bottom Mobile Container --}}
+    </div>
+    {{-- Footer Bottom Desktop --}}
+    <div class="footer-bottom-desktop text-uppercase">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="d-flex align-items-center justify-content-between">
+              <a href="#" class="btn-primary">Sign-up now!</a>
+              <div class="social-links d-flex align-items-center">
+                <h2>Follow us</h2>
+                <ul class="social-icons">
+                  <li>
+                    <a href="#">
+                      <img src="{{asset('img/footer-facebook.png')}}" alt="Facebook">
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <img src="{{asset('img/footer-twitter.png')}}" alt="Twitter">
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <img src="{{asset('img/footer-youtube.png')}}" alt="Youtube">
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <img src="{{asset('img/footer-pinterest.png')}}" alt="Pinterest">
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <img src="{{asset('img/footer-periscope.png')}}" alt="Periscope">
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>  {{-- Closing Footer Bottom Container --}}
+      </div>  {{-- Closing Footer Bottom Desktop Container --}}
+    </div>
   </div>
 </footer>
