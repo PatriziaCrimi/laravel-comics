a9d29ac31ee7fc1acd1285761a7e94709b8aa820
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-01-08 01:41:15 +0100

    Page "comics" (homepage) completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,75 @@
 
 @section('section')
   <section id="comics" class="text-center">
-    <div class="container">
-      <div class="row">
-        @foreach ($comics_list as $key => $album)
-          <div class="col-12">
-            <h2>{{$album['title']}}</h2>
-            <img src="{{$album['thumb']}}" alt="{{$album['title']}}">
+    {{-- Current Series --}}
+    <div class="current-series">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-left">
+            <h1>Current series</h1>
           </div>
-        @endforeach
-      </div>
-    </div>   {{-- Closing Section-Comics container --}}
+        </div>
+        <div class="cards-wrapper">
+          <div class="row">
+            @foreach ($comics_list as $key => $album)
+              <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+                <div class="card-album">
+                  <div class="thumb-album">
+                    <img src="{{$album['thumb']}}" alt="{{$album['series']}}">
+                  </div>
+                  <h2 class="text-left">{{$album['series']}}</h2>
+                </div>
+              </div>
+            @endforeach
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <button class="btn-primary text-uppercase" type="button" name="button">Load more</button>
+          </div>
+        </div>
+      </div>   {{-- Closing Current-series container --}}
+    </div>
+    {{-- Links --}}
+    <div class="links">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <ul class="d-flex justify-content-between">
+              <li>
+                <a href="#">
+                  <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="Digital comics">
+                  <span>Digital comics</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="Merchandise T-shirt">
+                  <span>De merchandise</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="Subscription cards">
+                  <span>Subscription</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="Locator">
+                  <span>Comic shop locator</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="Dc power visa box">
+                  <span>Dc power visa</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>  {{-- Closing Links container --}}
+    </div>
   </section>
 @endsection
